ee0e53b68f53a0264e523a121a3eeb9bfdc0e3de
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-04-28 16:27:54 +0900

    Profile 페이지에 텍스트, 이미지 캐러셀 컴포넌트 추가 & 스크롤뷰 추가

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,11 +1,89 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Text,
+  View,
+  useWindowDimensions,
+  StyleSheet,
+  ScrollView,
+} from 'react-native';
+import ImageCarousel from '../components/imagecarousel/ImageCarousel';
+import user from '../../model/user';
+import photo from '../../model/photo';
+import CustomText from '../components/text/CustomText';
+
+// const screenWidth = Math.round(Dimensions.get('window').width);
+
+const PAGES = [
+  {
+    num: 1,
+    color: '#86E3CE',
+  },
+  {
+    num: 2,
+    color: '#D0E6A5',
+  },
+  {
+    num: 3,
+    color: '#FFDD94',
+  },
+  {
+    num: 4,
+    color: '#FA897B',
+  },
+  {
+    num: 5,
+    color: '#CCABD8',
+  },
+];
+
+const styles = StyleSheet.create({
+  textWrapper: {
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+  },
+  userNameText: {
+    fontWeight: 'bold',
+    paddingVertical: 7,
+  },
+  userInfoText: {
+    color: '#555',
+    paddingBottom: 2,
+  },
+  userBriefIntro: {
+    paddingVertical: 7,
+  },
+});
 
 function Profile() {
+  const window = useWindowDimensions();
+
   return (
-    <View>
-      <Text>프로필이구요</Text>
-    </View>
+    <ScrollView>
+      <ImageCarousel pages={photo} pageWidth={window.width} />
+
+      <View style={styles.textWrapper}>
+        <CustomText
+          customTextType="large"
+          customTextStyle={styles.userNameText}>
+          {user.name}
+        </CustomText>
+        <CustomText
+          customTextType="small"
+          customTextStyle={styles.userInfoText}>
+          {user.phone}
+        </CustomText>
+        <CustomText
+          customTextType="small"
+          customTextStyle={styles.userInfoText}>
+          {user.email}
+        </CustomText>
+        <CustomText
+          customTextType="medium"
+          customTextStyle={styles.userBriefIntro}>
+          {user.briefIntro}
+        </CustomText>
+      </View>
+    </ScrollView>
   );
 }
 
