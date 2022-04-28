a02665bd637f431a8a166e78a9e59b82d291ae05
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-04-28 18:30:58 +0900

    내 프로필 탭의 중첩 네비게이션 때문에 stack navigator 추가

--- App.tsx
+++ App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {NavigationContainer, DefaultTheme} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Profile from './src/pages/Profile';
 import Careers from './src/pages/Careers';
 import Etc from './src/pages/Etc';
+
+import ProfileDetails from './src/pages/ProfileDetails';
 
 const Tab = createBottomTabNavigator();
 const navigationTheme = {
@@ -12,14 +14,24 @@ const navigationTheme = {
   colors: {...DefaultTheme.colors, background: '#fff'},
 };
 
+const ProfileStack = createNativeStackNavigator();
+function ProfileStackScreen() {
+  return (
+    <ProfileStack.Navigator>
+      <ProfileStack.Screen name="Profile" component={Profile} />
+      <ProfileStack.Screen name="ProfileDetails" component={ProfileDetails} />
+    </ProfileStack.Navigator>
+  );
+}
+
 function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
       <Tab.Navigator>
         <Tab.Screen
           name="Profile"
-          component={Profile}
-          options={{title: '내 프로필'}}
+          component={ProfileStackScreen}
+          options={{headerShown: false}}
         />
         <Tab.Screen
           name="Careers"
