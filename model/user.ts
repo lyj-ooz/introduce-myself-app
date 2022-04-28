d8b3d7e9eb5ebe98f319c14ae0bf33c5c79aad71
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-04-28 18:27:54 +0900

    리스트 활용하려고 user 데이터 일부를 배열 형식으로 변경

--- model/user.ts
+++ model/user.ts
@@ -3,7 +3,7 @@ interface IUser {
   phone: string;
   email: string;
   briefIntro: string;
-  longIntro: string;
+  longIntro: ILongIntro[];
   careers: IUserCareers[];
 }
 
@@ -12,13 +12,31 @@ interface IUserCareers {
   job: string;
 }
 
+interface ILongIntro {
+  title: string;
+  content: string;
+}
+
 const user: IUser = {
   name: '이유진',
   phone: '010-6471-5318',
   email: 'yjlee1026@yahoo.com',
   briefIntro:
     '나만의 기술을 가지고 싶다는 생각에 우연히 시작한 개발이 점점 재밌어서 계속 하는 중입니다. 프론트엔드 개발자로 시작했지만 제 역할에 제한을 두지 않고 백엔드, 모바일, pm, 디자인 등 개발 관련 분야 두루두루 경험하는 것을 지향합니다. 앞으로 10년 후에 어떤 개발자가 되어 있을지 모르지만 나이가 들어도 기꺼이 배울 수 있는 개발자가 되고 싶습니다.',
-  longIntro: '긴 소개글~~~~~~',
+  longIntro: [
+    {
+      title: '제목1',
+      content: '긴 이야기~~',
+    },
+    {
+      title: '제목2',
+      content: '긴 이야기~~',
+    },
+    {
+      title: '제목3',
+      content: '긴 이야기~~',
+    },
+  ],
   careers: [
     {
       companyName: '트레디',
