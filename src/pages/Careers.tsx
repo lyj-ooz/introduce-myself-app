340da3879811fa4c8ddd33b2c8f888ea4a84cc87
lyj-ooz <78289917+lyj-ooz@users.noreply.github.com>
2022-04-28 20:49:10 +0900

    두번쨰 탭 페이지 작성

--- src/pages/Careers.tsx
+++ src/pages/Careers.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {View, ScrollView, StyleSheet} from 'react-native';
+import user from '../../model/user';
+import CustomText from '../components/text/CustomText';
+
+const styles = StyleSheet.create({
+  careerWrapper: {
+    padding: 15,
+  },
+  companyName: {
+    fontWeight: 'bold',
+  },
+  job: {
+    fontWeight: '300',
+  },
+});
 
 function Careers() {
   return (
-    <View>
-      <Text>경력사항이구요</Text>
-    </View>
+    <ScrollView>
+      {user.careers.map(career => (
+        <View style={styles.careerWrapper} key={career.companyName}>
+          <CustomText
+            customTextType="large"
+            customTextStyle={styles.companyName}>
+            {`💼 ${career.companyName}`}
+          </CustomText>
+          <CustomText customTextType="medium" customTextStyle={styles.job}>
+            {career.job}
+          </CustomText>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
 
